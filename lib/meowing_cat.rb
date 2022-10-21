11bdc731cacec1bba4d76d7ad823a645d2f8cea0
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-21 19:44:03 +0300

    changes made

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+## code your solution here.
+## macros
+require 'pry'
+class Cat
+  attr_accessor :name
+#   def name=(name)
+#     @name = name
+#   end
+  def meow
+    puts "meow!"
+  end
+end
+c1 = Cat.new()
+c1.name = "Maseratti"
+
+# binding.pry
